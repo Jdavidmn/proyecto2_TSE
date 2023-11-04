439838142c714996ff8269bd29f79c5f0613f7fa
David <j.davidmn@hotmail.com>
2023-11-03 23:23:47 -0600

    Se agrega funcionalidad parcial para guardar los resultados

--- emociones/emotions.py
+++ emociones/emotions.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import json
+import time
+from datetime import datetime
 
 import numpy as np
 import cv2
@@ -43,32 +46,53 @@ def main():
 
     # start the webcam feed
     cap = cv2.VideoCapture(0)
+    begin = time.time()
+    working = True
+    fps = 1
     while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface'
-                                         '_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray, scaleFactor=1.3,
-                                          minNeighbors=5)
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10),
-                          (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_resized = cv2.resize(roi_gray, (48, 48))
-            cropped_img = np.expand_dims(np.expand_dims(roi_resized, -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
-                        2, cv2.LINE_AA)
+        try:
+            with open('settings.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                working = data['work']
+                fps = int(data['fps'])
+        except FileNotFoundError:
+            print("Configuracion no encontrada, ejecutando configuracion"
+                  " por defecto")
 
-        cv2.imshow('Video', cv2.resize(frame, (1600, 960),
-                                       interpolation=cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        actual_time = time.time() - begin
+
+        if working:
+            if actual_time >= fps:
+                # Find haar cascade to draw bounding box around face
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                facecasc = cv2.CascadeClassifier('haarcascade_frontalface'
+                                                 '_default.xml')
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = facecasc.detectMultiScale(gray, scaleFactor=1.3,
+                                                  minNeighbors=5)
+
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y-50), (x+w, y+h+10),
+                                  (255, 0, 0), 2)
+                    roi_gray = gray[y:y + h, x:x + w]
+                    roi_resized = cv2.resize(roi_gray, (48, 48))
+                    cropped_img = np.expand_dims(np.expand_dims(roi_resized, -1), 0)
+                    prediction = model.predict(cropped_img)
+                    maxindex = int(np.argmax(prediction))
+                    cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
+                                2, cv2.LINE_AA)
+
+                cv2.imshow('Video', cv2.resize(frame, (1600, 960),
+                                               interpolation=cv2.INTER_CUBIC))
+                cv2.imwrite(f'{datetime.now()}.png', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                begin = time.time()
+        else:
             break
 
     cap.release()
