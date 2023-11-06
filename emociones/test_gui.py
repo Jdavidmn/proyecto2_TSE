116a3b1aace7d23e11288b8b7fdbd383c3064a8a
David <j.davidmn@hotmail.com>
2023-11-05 23:16:58 -0600

    mas botones

--- emociones/test_gui.py
+++ emociones/test_gui.py
@@ -4,15 +4,23 @@ sg.theme('DarkGrey7')   # Add a touch of color
 # All the stuff inside your window.
 layout = [[sg.Text('Conexión: '),
            sg.Text('DESCONECTADO', key='sys_status'),
-           sg.Text('APLICACIÓN NO FUNCIONANDO', key='app_status')],
+           sg.Text('     '),
+           sg.Text('Funcionamiento: '),
+           sg.Text('ENCENDIDO', key='app_status')],
           [sg.Text('Tiempo de muestreo: '),
-           sg.Slider((1, 10), orientation='h', s=(10, 15))],
+           sg.Slider((1, 10), orientation='h', s=(10, 15)),
+           sg.Text('    '),
+           sg.Button('Encender aplicación')],
+          [sg.Text('               '),
+           sg.Button('Configurar'),
+           sg.Text('                             '),
+           sg.Button('Leer datos')],
           [sg.Button('Ok'),
            sg.Button('Cancel')]
           ]
 
 # Create the Window
-window = sg.Window('Detección de emociones', layout)
+window = sg.Window('Detector de emociones', layout)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -22,6 +30,6 @@ while True:
     if event in (sg.WIN_CLOSED, 'Cancel'):
         break
 
-    print('You entered ', values[0], values[1])
+    print('You entered ', values[0])
 
 window.close()
